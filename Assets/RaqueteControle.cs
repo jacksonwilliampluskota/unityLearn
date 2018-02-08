6d27ee2c2d2c2228bfce2587d5dfb2ea43264fe9
Jackson william Pluskota <jacksonw7007@hotmail.com>
2018-02-07 23:12:01 -0200

    init
    teste unity git

---
+++ Assets/RaqueteControle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaqueteControle : MonoBehaviour {
+	public Vector3 moveSpeed;
+	public float rotSpeed = 5f;
+
+	void Start () {
+		//Debug.Log("comecei");
+	}
+	
+	void Update () {
+		var hor = Input.GetAxis("Horizontal");
+		var ver = Input.GetAxis("Vertical");
+		transform.position += hor * moveSpeed * Time.deltaTime;
+
+		var rotZeuler = (ver * Vector3.forward) * rotSpeed * Time.deltaTime;
+
+		transform.rotation *= Quaternion.Euler(rotZeuler);
+	}
+}
